b76cd622fe8fe688aa6196a85314397a3551ac73
DEAN\Dean <ducthienweakt@gmail.com>
2023-06-17 12:48:49 +0700

    added strava user

--- src/app/welcome/connect-strava/connect-strava.component.ts
+++ src/app/welcome/connect-strava/connect-strava.component.ts
@@ -8,60 +8,67 @@ import { environment } from 'src/environments/environment';
   templateUrl: './connect-strava.component.html',
   styleUrls: ['./connect-strava.component.scss']
 })
-export class ConnectStravaComponent implements OnInit{
+export class ConnectStravaComponent implements OnInit {
 
   @Output() loggingStravaEvent = new EventEmitter<string>();
   constructor(
     private route: ActivatedRoute,
     private httpClient: HttpClient,
     private router: Router
-  ){
+  ) {
 
   }
 
   ngOnInit(): void {
     let code = this.route.snapshot.queryParamMap.get("code");
-    if(code){
+    if (code) {
       this.getStravaAccessToken(code);
     }
   }
 
-  onConnectStravaClick(event:any){
-    window.location.href = "http://www.strava.com/oauth/authorize?client_id="+
-    environment.starvaClientId+"&response_type=code&redirect_uri="+
-    environment.appUrl+"&approval_prompt=auto&scope=read,activity:read_all,activity:write&state=test";
-     
+  onConnectStravaClick(event: any) {
+    window.location.href = "http://www.strava.com/oauth/authorize?client_id=" +
+      environment.starvaClientId + "&response_type=code&redirect_uri=" +
+      environment.appUrl + "&approval_prompt=auto&scope=read,activity:read_all,activity:write&state=test";
+
   }
 
-  getStravaAccessToken(code: string){
-    this.httpClient.post(`https://www.strava.com/oauth/token`,{
-      client_id:environment.starvaClientId,
+  getStravaAccessToken(code: string) {
+    this.httpClient.post(`https://www.strava.com/oauth/token`, {
+      client_id: environment.starvaClientId,
       client_secret: environment.starvaClientSecret,
       code: code,
       grant_type: 'authorization_code'
-    }).subscribe(async (result:any)=>{
-      localStorage.setItem('accessToken',result.access_token);
+    }).subscribe(async (result: any) => {
+      localStorage.setItem('accessToken', result.access_token);
+      localStorage.setItem('stravaName', result.athlete.firstname + " " + result.athlete.lastname);
+      this.loggingStravaEvent.emit(result.access_token);
       let strava_id = result.athlete.id;
-       this.httpClient.get(environment.apiUrl+'/strava-users?filters[strava_id][$eq]='+strava_id).subscribe((stravaResponse:any)=>{
-        if(stravaResponse.data){
+      let data = {
+        access_token: result.access_token,
+        refresh_token: result.refresh_token,
+        strava_id: strava_id
+      }
+
+      this.httpClient.get(environment.apiUrl + '/strava-users?filters[strava_id][$eq]=' + strava_id).subscribe((stravaResponse: any) => {
+        // update strava token
+        if (stravaResponse.data && stravaResponse.data[0]) {
           let stravaUser = stravaResponse.data[0];
-          
-          localStorage.setItem('stravaName',result.athlete.firstname + " " + result.athlete.lastname);
-          let data = {
-            access_token:result.access_token,
-            refresh_token:result.refresh_token
-          }
-          this.httpClient.put(environment.apiUrl+'/strava-users/'+stravaUser.id,{data:data}).subscribe(saveSuccess=>{
-            this.loggingStravaEvent.emit(result.access_token);
+
+          this.httpClient.put(environment.apiUrl + '/strava-users/' + stravaUser.id, { data: data }).subscribe(saveSuccess => {
+            this.router.navigate(['/']);
+          })
+        } else {
+          this.httpClient.post(environment.apiUrl + '/strava-users/', { data: data }).subscribe(saveSuccess => {
             this.router.navigate(['/']);
           })
         }
-       });
-    
+      });
+
 
       //update token
 
-      
+
     });
 
   }
