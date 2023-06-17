e1b2d9ea2f5c6eb519ceb3d6467ba40b4f80dd66
DEAN\Dean <ducthienweakt@gmail.com>
2023-06-17 12:58:57 +0700

    added production build

---
+++ src/environments/environment prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    appUrl: 'http://chaydi.fun',
+    apiUrl: 'https://chaydi-524abe5b3fd9.herokuapp.com/api',
+    botThoApi: 'http://chaydi.fun//botApi',
+    stravaConfig: {
+      "access_token"  : '',
+      "client_id"     : "108204",
+      "client_secret" : "b9b073e3cb73323f7d47e6ab9783bbeea366839e",
+      "redirect_uri"  : "http://chaydi.fun/exchange_token",
+      "subscriptions_callback_url":"https://chaydi-524abe5b3fd9.herokuapp.com/api/strava-webhook",
+      "apiUrl": "https://www.strava.com/api/v3"
+    },
+    starvaClientId:'108204',
+    starvaClientSecret:'b9b073e3cb73323f7d47e6ab9783bbeea366839e'
+}
