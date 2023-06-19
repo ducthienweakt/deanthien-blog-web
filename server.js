f4a076289c06ad032e3d2aa045cf4ed90d1e2067
DEAN\Dean <ducthienweakt@gmail.com>
2023-06-19 12:43:53 +0700

    more changes

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/chaydi'));
 
+
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/chaydi/'}),
+);
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -16,10 +22,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/chaydi/'}),
-);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
