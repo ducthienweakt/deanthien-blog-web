4e63532d694b8ae1a360ef6d260acf76c5b17e16
DEAN\Dean <ducthienweakt@gmail.com>
2023-06-19 12:46:28 +0700

    more changes

--- server.js
+++ server.js
@@ -13,15 +13,9 @@ app.use(express.static('./dist/chaydi'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/chaydi/'}),
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+    res.header("Access-Control-Allow-Origin", '*')
 );
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
