7365ec4a885d1673304331d07813c4dedea99608
DEAN\Dean <ducthienweakt@gmail.com>
2023-06-19 12:05:41 +0700

    update cors

--- server.js
+++ server.js
@@ -6,25 +6,7 @@ const cors = require('cors');
 const app = express();
 
 // add this code
-const whitelist = ['http://thomay.vn']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
+app.options('*', cors()) // include before other routes
 
 
 // Serve only the static files form the dist directory
